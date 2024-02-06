e62434d7d183dd47d000016226fddbd3cf1042e3
Xumpocmb <x.xumpocmb@gmail.com>
2024-02-06 21:48:45 +0300

    catalog page pagination done

--- catalog_app/views.py
+++ catalog_app/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_list_or_404
 from catalog_app.models import Category, Product
+from django.core.paginator import Paginator
 
 
 def catalog(request, slug=None):
+
     if slug:
         products = Product.objects.filter(category__slug=slug)
     else:
         products = Product.objects.all()
+
+    page = int(request.GET.get('page', 1))
+    paginator = Paginator(products, 6)
+    current_page = paginator.page(page)
+
     context = {
         'title': 'Каталог',
         'categories': Category.objects.all(),
-        'products': products,
+        'products': current_page,
     }
     return render(request, 'catalog_app/menu.html', context=context)
