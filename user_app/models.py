cdbac6c7dd7a152485779847d57ab377bb0c84a4
Xumpocmb <x.xumpocmb@gmail.com>
2024-02-09 10:56:42 +0300

    user model is ready

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', verbose_name='Изображение', null=True, blank=True)
+    phone = models.CharField(max_length=9, verbose_name='Телефон', null=True, blank=True)
+    address = models.CharField(max_length=255, verbose_name='Адрес', null=True, blank=True)
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.username
+
+    def display_id(self):
+        return f'ID: {self.id:05}'
